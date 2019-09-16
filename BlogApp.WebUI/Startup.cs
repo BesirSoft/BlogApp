87badbfcb491b46cc28e104bcb2df5adc99db9a5
alieryaman <postauyelik@gmail.com>
2019-09-16 18:39:36 +0300

    Proje katmnları oluşturuldu

--- BlogApp.WebUI/Startup.cs
+++ BlogApp.WebUI/Startup.cs
@@ -48,7 +48,7 @@ namespace BlogApp.WebUI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseStatusCodePages();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
